100fdc18bac0297239d9bab7222b57a793c2ccc0
MarianaPizarro14 <maripizarro1418@gmail.com>
2026-05-22 12:11:49 -0500

    fix: corrijo error de sintaxis en debug.py

--- debug.py
+++ debug.py
@@ -4,8 +4,10 @@
 numeros = [10, 20, 30, 40, 50]
 
 total = 0
-for n in numeros
+for n in numeros:
     total = total + n
 
 print(f"La suma es: {total}")
 print(f"El promedio es: {total / len(numeros)}")
+
+print(numeros)
--- saludo.py
+++ saludo.py
@@ -5,6 +5,6 @@ nombre  = "Mariana Pizarro"
 carrera = "Análisis y Desarrollo de Software"
 proyecto = "Uy, era para hoy"
 
-print(f"Hola, soy {Mariana Pizarro}")
+print(f"Hola, soy {Mariana_Pizarro}")
 print(f"Estudio {ADSO} en el SENA")
-print(f"Mi proyecto es: {Uy, era para hoy}")
+print(f"Mi proyecto es: {Uy,era_para_hoy}")
